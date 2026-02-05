ffd9ea59174888dacea480a2458c1ac7fb2093b3
Vinayasena Reddy Katta <vinaysena.dev@gmail.com>
2026-02-05 15:21:26 +0530

    Welcome Screen
    implemented the welcome screen

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <section id="welcome">
+      <p className="text-5xl text-center">
+        Hey, I'm <b>Vinay sena</b>!
+      </p>
+      <p></p>
+      <h1 className="mt-1 text-5xl text-center">Welcome to my portfolio</h1>
+      <div className="small-screen">
+        <p>This portfolio is designed for desktops/tablets</p>
+      </div>
+    </section>
+  );
+};
+
+export default Welcome;
